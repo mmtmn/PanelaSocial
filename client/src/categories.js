ca1916fda62915fc33e8ac1666b5b202d06215e6
thiago <thiagomnobrega@gmail.com>
2019-12-25 20:47:13 -0300

    Update categories

--- client/src/categories.js
+++ client/src/categories.js
@@ -1,10 +1,10 @@
 const categories = [
-  'music',
-  'funny',
-  'videos',
-  'programming',
-  'news',
-  'fashion'
+  'Campinas',
+  'Barão Geraldo',
+  'Cidade Universitária I',
+  'Cidade Universitária II',
+  'Real Parque',
+  'Jardim Santa Genebra'
 ];
 
 export default categories;
